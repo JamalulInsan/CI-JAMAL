7444fc1f8215d5b23aa2f8be31a4748c0642c3bc
jamalul insan <jamalulinsan05@gmail.com>
2020-12-13 11:34:14 +0700

    autoload model view di ubah

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Control'=>'call','View','view');
+$autoload['model'] = array('Control'=>'call');
